78affb6adbc981f2ad09bbaf1055331637178fc4
Laumla <dackyyt1999@gmail.com>
2026-03-04 11:45:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,56 +1,89 @@
-function moveWindows() {
+let box1, box2;
+let smallMode = false;
+let created = false;
 
-  // Skapa två popup-fönster
-  let window1 = window.open(location.href, "_blank", "width=200,height=200");
-  let window2 = window.open(location.href, "_blank", "width=200,height=200");
+// Skapa overlay-rutor
+function createWindows() {
+  box1 = document.createElement("div");
+  box2 = document.createElement("div");
 
-  // Om popup blockerad
-  if (!window1 || !window2) {
-    document.getElementById("message").innerText =
-      "Popup blockerare är aktiverad! Tillåt popups och försök igen.";
-    return;
-  }
+  [box1, box2].forEach(box => {
+    box.style.position = "fixed";
+    box.style.width = "200px";
+    box.style.height = "200px";
+    box.style.background = "red";
+    box.style.zIndex = "9999";
+    box.style.pointerEvents = "none"; // så de inte blockerar klick
+    document.body.appendChild(box);
+  });
 
-  // Startpositioner
-  let window1X = 100;
-  let window1Y = 100;
-  let window1VX = 3;
-  let window1VY = 3;
+  box1.style.left = "100px";
+  box1.style.top = "100px";
 
-  let window2X = 300;
-  let window2Y = 200;
-  let window2VX = -3;
-  let window2VY = 3;
+  box2.style.left = "400px";
+  box2.style.top = "200px";
+
+  moveBoxes();
+  created = true;
+}
+
+// Rörelse
+let x1 = 100, y1 = 100, vx1 = 3, vy1 = 3;
+let x2 = 400, y2 = 200, vx2 = -3, vy2 = 3;
+
+function moveBoxes() {
 
   function frame() {
 
-    // Flytta window 1
-    window1X += window1VX;
-    window1Y += window1VY;
-    window1.moveTo(window1X, window1Y);
+    x1 += vx1;
+    y1 += vy1;
 
-    // Studsa
-    if (window1X <= 0 || window1X >= screen.width - 200) {
-      window1VX *= -1;
-    }
-    if (window1Y <= 0 || window1Y >= screen.height - 200) {
-      window1VY *= -1;
-    }
+    x2 += vx2;
+    y2 += vy2;
 
-    // Flytta window 2
-    window2X += window2VX;
-    window2Y += window2VY;
-    window2.moveTo(window2X, window2Y);
+    if (x1 <= 0 || x1 >= window.innerWidth - box1.offsetWidth) vx1 *= -1;
+    if (y1 <= 0 || y1 >= window.innerHeight - box1.offsetHeight) vy1 *= -1;
 
-    if (window2X <= 0 || window2X >= screen.width - 200) {
-      window2VX *= -1;
-    }
-    if (window2Y <= 0 || window2Y >= screen.height - 200) {
-      window2VY *= -1;
-    }
+    if (x2 <= 0 || x2 >= window.innerWidth - box2.offsetWidth) vx2 *= -1;
+    if (y2 <= 0 || y2 >= window.innerHeight - box2.offsetHeight) vy2 *= -1;
+
+    box1.style.left = x1 + "px";
+    box1.style.top = y1 + "px";
+
+    box2.style.left = x2 + "px";
+    box2.style.top = y2 + "px";
 
     requestAnimationFrame(frame);
   }
 
   frame();
 }
+
+// Gör rutorna små
+function toggleSize() {
+  smallMode = !smallMode;
+
+  if (smallMode) {
+    box1.style.width = "50px";
+    box1.style.height = "50px";
+    box2.style.width = "50px";
+    box2.style.height = "50px";
+  } else {
+    box1.style.width = "200px";
+    box1.style.height = "200px";
+    box2.style.width = "200px";
+    box2.style.height = "200px";
+  }
+}
+
+// Starta automatiskt när sidan laddas
+window.onload = function () {
+  createWindows();
+};
+
+// Klicka var som helst för att ändra storlek
+document.addEventListener("click", function () {
+  if (created) {
+    toggleSize();
+  }
+});
